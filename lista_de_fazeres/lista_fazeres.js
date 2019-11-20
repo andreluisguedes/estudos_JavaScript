e45edd69180a1af272ece98ce792471f282b7579
andreluisguedes <andrebni@yahoo.com.br>
2019-11-20 12:07:50 -0300

    Created function to add and delete to do

--- lista_de_fazeres/lista_fazeres.js
+++ lista_de_fazeres/lista_fazeres.js
@@ -2,3 +2,45 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
+var fazeres = [
+    'Preparar café',
+    'Agoar as plantas',
+    'Tomar banho'
+]
+
+function renderFazeres() {
+    listElement.innerHTML = '';
+    
+    for (const [pos, tarefa] of fazeres.entries()){
+        var tarefaElement = document.createElement('li');
+        var tarefaText = document.createTextNode(tarefa);
+
+        var linkElement = document.createElement('button');
+        var linkText = document.createTextNode('Excluir');
+        linkElement.appendChild(linkText)
+        
+        linkElement.setAttribute('onclick', 'excluirTarefa('+ pos +')')
+
+        tarefaElement.appendChild(tarefaText)
+        tarefaElement.appendChild(linkElement)
+
+        listElement.appendChild(tarefaElement)
+    }
+}
+
+renderFazeres();
+
+function addTarefa() {
+    var tarefaText = inputElement.value;
+
+    fazeres.push(tarefaText);
+    inputElement.value = '';
+    renderFazeres();
+}
+
+buttonElement.onclick = addTarefa
+
+function excluirTarefa(pos) {
+    fazeres.splice(pos, 1);
+    renderFazeres();
+}
