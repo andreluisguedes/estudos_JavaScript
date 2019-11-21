a5a29d96ec658e2df7252c0c3be4af8f4d109d6b
andreluisguedes <andrebni@yahoo.com.br>
2019-11-21 12:38:03 -0300

    Increase LocalStorage

--- lista_de_fazeres/lista_fazeres.js
+++ lista_de_fazeres/lista_fazeres.js
@@ -2,14 +2,17 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var fazeres = [
-    'Preparar café',
-    'Agoar as plantas',
-    'Tomar banho'
-]
+// var fazeres = [
+//     'Preparar café',
+//     'Agoar as plantas',
+//     'Tomar banho'
+// ]
+
+var fazeres = JSON.parse(localStorage.getItem('listTarefas')) || [];
 
 function renderFazeres() {
-    listElement.innerHTML = '';
+    listElement.innerHTML = '';     //zera o html antes de renderizar para não renderizar duas vezes a mesma lista
+    
     
     for (const [pos, tarefa] of fazeres.entries()){
         var tarefaElement = document.createElement('li');
@@ -18,7 +21,7 @@ function renderFazeres() {
         var linkElement = document.createElement('button');
         var linkText = document.createTextNode('Excluir');
         linkElement.appendChild(linkText)
-        
+
         linkElement.setAttribute('onclick', 'excluirTarefa('+ pos +')')
 
         tarefaElement.appendChild(tarefaText)
@@ -36,6 +39,7 @@ function addTarefa() {
     fazeres.push(tarefaText);
     inputElement.value = '';
     renderFazeres();
+    saveFazeres();
 }
 
 buttonElement.onclick = addTarefa
@@ -43,4 +47,9 @@ buttonElement.onclick = addTarefa
 function excluirTarefa(pos) {
     fazeres.splice(pos, 1);
     renderFazeres();
+    saveFazeres();
+}
+
+function saveFazeres() {
+    localStorage.setItem('listTarefas', JSON.stringify(fazeres));
 }
